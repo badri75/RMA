8c64c564d8efa046081498854705bd70d2fa7278
Badri SF010 <rbadri7501@gmail.com>
2022-07-04 19:55:08 +0530

    correct-setstate

--- src/Login.js
+++ src/Login.js
@@ -3,6 +3,30 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {Button, Form} from 'react-bootstrap';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            pwd: '', 
+            submitDisabled: true       // separate flag for submit
+        }
+    }
+    handleChangeEmail(e) {
+        this.setState({email: e.target.value});
+        this.submitCheck();
+    }
+    handleChangeText(e) {
+        this.setState({pwd: e.target.value});
+        this.submitCheck();
+    }
+    submitCheck() {
+        if (this.state.email.length > 0 && this.state.pwd.length > 8) {
+            this.setState({submitDisabled: false});
+        } else {    
+            this.setState({submitDisabled: true});
+        }
+        console.log(this.state.email);
+    }
     render(){
         return(
             <div style={{marginTop: '230px',}}>
@@ -10,20 +34,22 @@ class Login extends Component {
                 <Form style={{padding: '15px', border: '2px solid black', backgroundColor: "rgba(255,250,250, 0.8)"}}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" name="email"/>
+                        <Form.Control type="email" placeholder="Enter email" name="email" 
+                        required={true} onChange={this.handleChangeEmail}/>
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" name="password"/>
+                        <Form.Control type="password" placeholder="Password" name="password" 
+                        required={true} onChange={this.handleChangeText}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
+                        <Form.Check type="checkbox" label="Terms & Conditions" required={true} />
                     </Form.Group>
                     <div style={{display: 'flex', justifyContent: 'center'}}>
-                        <Button variant="primary" type="submit">Submit</Button>
+                        <Button variant="primary" type="submit"  disabled={this.state.submitDisabled}>Submit</Button>
                     </div>
                 </Form>
             </div>
