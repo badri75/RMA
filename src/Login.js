844cf78b09199cd8707854fe504024b00ac8ef79
Badri Narayanan <rbadri7501@gmail.com>
2022-07-05 20:31:08 +0530

    password-valid-unsuccess

--- src/Login.js
+++ src/Login.js
@@ -1,57 +1,119 @@
-import React, {Component} from "react";
+import React, {Component, useState} from "react";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {Button, Form} from 'react-bootstrap';
 
-class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            pwd: '', 
-            submitDisabled: true       // separate flag for submit
+function Login(){
+    const [state, setState] = useState({
+        username: '',
+        password: '',
+        submitDisabled: true,
+    });
+    let handleChangeEmail = (event) => {
+        setState({
+            ...state,
+            username: event.target.value,
+        });
+        submitCheck();
+    }
+    let handleChangePassword = (event) => {
+        setState({
+            ...state,
+            password: event.target.value,
+        });
+        submitCheck();
+    }
+    function submitCheck() {
+        if (state.username.length > 0 && state.password.length > 8) {
+            setState({
+                ...state,
+                submitDisabled: false,
+            });
+        }
+        else {
+            setState({
+                ...state,
+                submitDisabled: true,
+            });
         }
     }
-    handleChangeEmail(e) {
-        this.setState({email: e.target.value});
-        this.submitCheck();
-    }
-    handleChangeText(e) {
-        this.setState({pwd: e.target.value});
-        this.submitCheck();
-    }
-    submitCheck() {
-        if (this.state.email.length > 0 && this.state.pwd.length > 8)
-            this.setState({submitDisabled: false});
-    }
-    render(){
-        return(
-            <div style={boxStyle}>
-                <h4 style={{display: 'flex', justifyContent: 'center'}}>Real Madrid CF</h4>
-                <Form style={formStyle}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" name="email" 
-                        required={true} onChange={this.handleChangeEmail.bind(this)}/>
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" name="password" 
-                        required={true} onChange={this.handleChangeText.bind(this)}/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Terms & Conditions" required={true} />
-                    </Form.Group>
-                    <div style={{display: 'flex', justifyContent: 'center'}}>
-                        <Button variant="primary" type="submit"  disabled={this.state.submitDisabled}>Submit</Button>
-                    </div>
-                </Form>
-            </div>
-        );
-    }
+    return (
+        <div style={boxStyle}>
+            <h4 style={{display: 'flex', justifyContent: 'center'}}>Real Madrid CF</h4>
+            <Form style={formStyle}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" name="email" 
+                    required={true} onChange={handleChangeEmail}/>
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" name="password" 
+                    required={true} onChange={handleChangePassword}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Terms & Conditions" required={true} />
+                </Form.Group>
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <Button variant="primary" type="submit"  disabled={state.submitDisabled}>Submit</Button>
+                </div>
+            </Form>
+        </div>
+    );
 }
+
+// class Login extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             email: '',
+//             pwd: '', 
+//             submitDisabled: true       // separate flag for submit
+//         }
+//     }
+//     handleChangeEmail(e) {
+//         this.setState({email: e.target.value});
+//         this.submitCheck();
+//     }
+//     handleChangeText(e) {
+//         this.setState({pwd: e.target.value});
+//         this.submitCheck();
+//     }
+//     submitCheck() {
+//         if (this.state.email.length > 0 && this.state.pwd.length > 8)
+//             this.setState({submitDisabled: false});
+//     }
+//     render(){
+//         return(
+//             <div style={boxStyle}>
+//                 <h4 style={{display: 'flex', justifyContent: 'center'}}>Real Madrid CF</h4>
+//                 <Form style={formStyle}>
+//                     <Form.Group className="mb-3" controlId="formBasicEmail">
+//                         <Form.Label>Email address</Form.Label>
+//                         <Form.Control type="email" placeholder="Enter email" name="email" 
+//                         required={true} onChange={this.handleChangeEmail.bind(this)}/>
+//                         <Form.Text className="text-muted">
+//                             We'll never share your email with anyone else.
+//                         </Form.Text>
+//                     </Form.Group>
+//                     <Form.Group className="mb-3" controlId="formBasicPassword">
+//                         <Form.Label>Password</Form.Label>
+//                         <Form.Control type="password" placeholder="Password" name="password" 
+//                         required={true} onChange={this.handleChangeText.bind(this)}/>
+//                     </Form.Group>
+//                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
+//                         <Form.Check type="checkbox" label="Terms & Conditions" required={true} />
+//                     </Form.Group>
+//                     <div style={{display: 'flex', justifyContent: 'center'}}>
+//                         <Button variant="primary" type="submit"  disabled={this.state.submitDisabled}>Submit</Button>
+//                     </div>
+//                 </Form>
+//             </div>
+//         );
+//     }
+// }
 
 const boxStyle = {
     marginTop: '45%',
