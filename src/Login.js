ecbe2867eeb32ae4eaef8ddc557098476eaa7a0c
Badri SF010 <rbadri7501@gmail.com>
2022-07-13 17:47:33 +0530

    added-inneHTML

--- src/Login.js
+++ src/Login.js
@@ -23,12 +23,20 @@ function Login(){
         let password = document.getElementById('pwd').value;
         if (mail && password.length>=8 && checkCaptcha()===true)
             alert('Login Successful');
-        else if (!mail)
-            alert('Invalid mail');
-        else if(password.length<8)
-            alert('Password must be at least 8 characters long');
-        else if (checkCaptcha()===false)
-            alert('Please enter the correct captcha');
+        else{
+            if (!mail)
+                document.getElementById('mail-valid').innerHTML = "<Form.Label class='text-danger'>Enter valid mail id<Form.Label>"
+            else
+                document.getElementById('mail-valid').innerHTML = ""
+            if (password.length<8)
+                document.getElementById('pwd-valid').innerHTML = "<Form.Label class='text-danger'>Password must be atleast be 8 Characters long<Form.Label>"
+            else
+                document.getElementById('pwd-valid').innerHTML = ""
+            if (checkCaptcha()===false)
+                document.getElementById('captcha-valid').innerHTML = "<Form.Label class='text-danger'>Invalid Captcha<Form.Label>"
+            else
+                document.getElementById('captcha-valid').innerHTML = ""
+        }
     }
 
     return (
@@ -39,17 +47,20 @@ function Login(){
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" name="email" 
                     required={true} id="mail"/>
+                    <span id='mail-valid'></span>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" placeholder="Password" name="password" 
                     required={true} id="pwd"/>
+                    <span id='pwd-valid'></span>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCaptcha">
                     <Form.Label>Captcha</Form.Label>
                     <LoadCanvasTemplate />
                     <Form.Control type="text" placeholder="Enter Captcha" name="captcha" 
                     required={true} id="captcha"/>
+                    <span id='captcha-valid'></span>
                 </Form.Group>
                 <div style={{display: 'flex', justifyContent: 'center'}}>
                     <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
