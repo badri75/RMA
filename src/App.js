0e757d9f1374df4618225af23cab97bba8902c07
Badri Narayanan <rbadri7501@gmail.com>
2022-07-31 20:45:48 +0530

    state-and-sep-dir

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Login from './Login';
+import Login from './components/login';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col} from 'react-bootstrap';
 import background from "./images/sanber.jpg";
